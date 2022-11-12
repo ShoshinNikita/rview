ef3a2c8d7b9c5d3446c297e9844e9d59ad49e2db
Nikita Shoshin <shoshin_nikita@fastmail.com>
2022-11-12 11:27:47 +0400

    increase default cache max age to 180 days

--- config/config.go
+++ config/config.go
@@ -52,12 +52,12 @@ func Parse() (cfg Config, err error) {
 	flag.StringVar(&cfg.RcloneTarget, "rclone-target", "", "rclone target")
 	//
 	flag.BoolVar(&cfg.Resizer, "resizer", true, "enable or disable image resizer")
-	flag.DurationVar(&cfg.ResizerMaxAge, "resizer-max-age", 60*24*time.Hour, "max age of resized images")
+	flag.DurationVar(&cfg.ResizerMaxAge, "resizer-max-age", 180*24*time.Hour, "max age of resized images")
 	flag.Int64Var(&cfg.ResizerMaxTotalSize, "resizer-max-total-size", 200<<20, "max total size of resized images, bytes")
 	flag.IntVar(&cfg.ResizerWorkersCount, "resizer-workers-count", runtime.NumCPU(), "number of image resize workers")
 	//
 	flag.BoolVar(&cfg.WebCache, "web-cache", true, "enable or disable web cache")
-	flag.DurationVar(&cfg.WebCacheMaxAge, "web-cache-max-age", 60*24*time.Hour, "max age of web cache")
+	flag.DurationVar(&cfg.WebCacheMaxAge, "web-cache-max-age", 180*24*time.Hour, "max age of web cache")
 	flag.Int64Var(&cfg.WebCacheMaxTotalSize, "web-cache-max-total-size", 200<<20, "max total size of web cache, bytes")
 	//
 	flag.BoolVar(&cfg.DebugLogLevel, "debug-log-level", false, "display debug log messages")
